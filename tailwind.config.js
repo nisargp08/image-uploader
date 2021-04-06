1aab80a524748f79c42cb4d8dda5524b8fc89be0
Nisarg Patel <nisargp42@gmail.com>
2021-04-06 01:42:17 -0400

    Design :: Ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow : {
+        'card-shadow' : '0px 4px 12px rgba(0, 0, 0, 0.1)'
+      },
+      fontSize : {
+        'xxs' : '10px'
+      }
+    },
   },
   variants: {
     extend: {},
